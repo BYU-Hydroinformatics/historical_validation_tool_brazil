93307b0467c50b22cf8ddf5695f43cffc10834ef
Jorge Luis Sanchez <JorgeLuis@Civils-MacBook-Pro.local>
2021-01-09 20:12:20 -0700

    Reducing functions

--- tethysapp/historical_validation_tool_brazil/app3.py
+++ tethysapp/historical_validation_tool_brazil/app3.py
@@ -27,71 +27,79 @@ class HistoricalValidationToolBrazil(TethysAppBase):
             UrlMap(
                 name='home',
                 url='historical-validation-tool-brazil',
-                controller='historical_validation_tool_brazil.controllers3.home'),
+                controller='historical_validation_tool_brazil.controllers.home'),
             UrlMap(
-                name='get_popup_response',
-                url='get-request-data',
-                controller='historical_validation_tool_brazil.controllers3.get_popup_response'),
+                name='get_discharge_data',
+                url='get-discharge-data',
+                controller='historical_validation_tool_brazil.controllers.get_discharge_data'),
+            UrlMap(
+                name='get_simulated_data',
+                url='get-simulated-data',
+                controller='historical_validation_tool_brazil.controllers.get_simulated_data'),
+            UrlMap(
+                name='get_simulated_bc_data',
+                url='get-simulated-bc-data',
+                controller='historical_validation_tool_brazil.controllers.get_simulated_bc_data'),
             UrlMap(
                 name='get_hydrographs',
                 url='get-hydrographs',
-                controller='historical_validation_tool_brazil.controllers3.get_hydrographs'),
+                controller='historical_validation_tool_brazil.controllers.get_hydrographs'),
             UrlMap(
                 name='get_dailyAverages',
                 url='get-dailyAverages',
-                controller='historical_validation_tool_brazil.controllers3.get_dailyAverages'),
+                controller='historical_validation_tool_brazil.controllers.get_dailyAverages'),
             UrlMap(
                 name='get_monthlyAverages',
                 url='get-monthlyAverages',
-                controller='historical_validation_tool_brazil.controllers3.get_monthlyAverages'),
+                controller='historical_validation_tool_brazil.controllers.get_monthlyAverages'),
             UrlMap(
                 name='get_scatterPlot',
                 url='get-scatterPlot',
-                controller='historical_validation_tool_brazil.controllers3.get_scatterPlot'),
+                controller='historical_validation_tool_brazil.controllers.get_scatterPlot'),
             UrlMap(
                 name='get_scatterPlotLogScale',
                 url='get-scatterPlotLogScale',
-                controller='historical_validation_tool_brazil.controllers3.get_scatterPlotLogScale'),
+                controller='historical_validation_tool_brazil.controllers.get_scatterPlotLogScale'),
             UrlMap(
                 name='get_volumeAnalysis',
                 url='get-volumeAnalysis',
-                controller='historical_validation_tool_brazil.controllers3.get_volumeAnalysis'),
+                controller='historical_validation_tool_brazil.controllers.get_volumeAnalysis'),
             UrlMap(
                 name='volume_table_ajax',
                 url='volume-table-ajax',
-                controller='historical_validation_tool_brazil.controllers3.volume_table_ajax'),
+                controller='historical_validation_tool_brazil.controllers.volume_table_ajax'),
             UrlMap(
                 name='make_table_ajax',
                 url='make-table-ajax',
-                controller='historical_validation_tool_brazil.controllers3.make_table_ajax'),
+                controller='historical_validation_tool_brazil.controllers.make_table_ajax'),
             UrlMap(
                 name='get-time-series',
                 url='get-time-series',
-                controller='historical_validation_tool_brazil.controllers3.get_time_series'),
+                controller='historical_validation_tool_brazil.controllers.get_time_series'),
             UrlMap(
                 name='get-time-series-bc',
                 url='get-time-series-bc',
-                controller='historical_validation_tool_brazil.controllers3.get_time_series_bc'),
+                controller='historical_validation_tool_brazil.controllers.get_time_series_bc'),
             UrlMap(
                 name='get_observed_discharge_csv',
                 url='get-observed-discharge-csv',
-                controller='historical_validation_tool_brazil.controllers3.get_observed_discharge_csv'),
+                controller='historical_validation_tool_brazil.controllers.get_observed_discharge_csv'),
             UrlMap(
                 name='get_simulated_discharge_csv',
                 url='get-simulated-discharge-csv',
-                controller='historical_validation_tool_brazil.controllers3.get_simulated_discharge_csv'),
+                controller='historical_validation_tool_brazil.controllers.get_simulated_discharge_csv'),
             UrlMap(
                 name='get_simulated_bc_discharge_csv',
                 url='get-simulated-bc-discharge-csv',
-                controller='historical_validation_tool_brazil.controllers3.get_simulated_bc_discharge_csv'),
+                controller='historical_validation_tool_brazil.controllers.get_simulated_bc_discharge_csv'),
             UrlMap(
                 name='get_forecast_data_csv',
                 url='get-forecast-data-csv',
-                controller='historical_validation_tool_brazil.controllers3.get_forecast_data_csv'),
+                controller='historical_validation_tool_brazil.controllers.get_forecast_data_csv'),
             UrlMap(
                 name='get_forecast_bc_data_csv',
                 url='get-forecast-bc-data-csv',
-                controller='historical_validation_tool_brazil.controllers3.get_forecast_bc_data_csv'),
+                controller='historical_validation_tool_brazil.controllers.get_forecast_bc_data_csv'),
         )
 
         return url_maps
